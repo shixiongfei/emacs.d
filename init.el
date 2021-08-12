ccbf98422a823c5691bd0b254cb120d2347640ea
shixiongfei <xiongfei.shi@icloud.com>
2021-08-12 20:05:48 +0800

    Add Scheme

--- init.el
+++ init.el
@@ -354,7 +354,8 @@
 (use-package rainbow-delimiters
   :ensure t
   :config
-  (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode))
+  (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'scheme-mode-hook #'rainbow-delimiters-mode))
 
 (use-package rainbow-mode
   :ensure t
@@ -542,6 +543,70 @@
             (concat common-lisp-hyperspec-root "Data/Map_Sym.txt"))
       (setq common-lisp-hyperspec-issuex-table
             (concat common-lisp-hyperspec-root "Data/Map_IssX.txt")))))
+
+;; Scheme
+(use-package cmuscheme
+  :init
+  (setq scheme-mit-dialect nil)
+
+  (if (eq system-type 'darwin)
+      (setq scheme-program-name "chez")
+    (setq scheme-program-name "scheme"))
+  :config
+  ;; bypass the interactive question and start the default interpreter
+  (defun scheme-proc ()
+    "Return the current Scheme process, starting one if necessary."
+    (unless (and scheme-buffer
+                 (get-buffer scheme-buffer)
+                 (comint-check-proc scheme-buffer))
+      (save-window-excursion
+        (run-scheme scheme-program-name)))
+    (or (scheme-get-process)
+        (error "No current process.  See variable `scheme-buffer'")))
+
+  (defun scheme-split-window ()
+    (cond
+     ((= 1 (count-windows))
+      (delete-other-windows)
+      (split-window-vertically (floor (* 0.68 (window-height))))
+      (other-window 1)
+      (switch-to-buffer "*scheme*")
+      (other-window 1))
+     ((not (get-buffer-window "*scheme*"))
+      (other-window 1)
+      (switch-to-buffer "*scheme*")
+      (other-window -1))))
+
+  (defun scheme-send-last-sexp-split-window ()
+    (interactive)
+    (scheme-split-window)
+    (scheme-send-last-sexp))
+
+  (defun scheme-send-definition-split-window ()
+    (interactive)
+    (scheme-split-window)
+    (scheme-send-definition))
+
+  (add-hook 'scheme-mode-hook
+            (lambda ()
+              (define-key scheme-mode-map (kbd "C-x C-e") 'scheme-send-last-sexp-split-window)
+              (define-key scheme-mode-map (kbd "C-c C-e") 'scheme-send-definition-split-window)))
+
+  (add-hook 'inferior-scheme-mode-hook
+            (lambda ()
+              ;; Overwrite the standard 'switch-to-buffer' to use
+              ;; 'switch-to-buffer-other-window'
+              (defun switch-to-scheme (eob-p)
+                "Switch to the Scheme process buffer.
+                 With argument, position cursor at end of buffer."
+                (interactive "P")
+                (if (or (and scheme-buffer (get-buffer scheme-buffer))
+                        (scheme-interactively-start-process))
+                    (switch-to-buffer-other-window scheme-buffer)
+                  (error "No current process buffer.  See variable `scheme-buffer'"))
+                (when eob-p
+                  (push-mark)
+                  (goto-char (point-max)))))))
 
 
 ;; Fonts setting
