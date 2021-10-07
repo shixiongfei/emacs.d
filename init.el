01797e11c53a9d1855966b9fda58cab063726b94
shixiongfei <xiongfei.shi@icloud.com>
2021-10-07 22:52:05 +0800

    Add Racket Mode

--- init.el
+++ init.el
@@ -621,6 +621,30 @@
   (add-hook 'lfe-mode-hook #'paredit-mode)
   (add-hook 'lfe-mode-hook #'rainbow-delimiters-mode))
 
+;; Racket
+(use-package racket-mode
+  :ensure t
+  :custom
+  (racket-memory-limit 8192)
+  :config
+  (with-eval-after-load 'racket-mode
+    (define-key racket-mode-map (kbd "M-RET") #'racket-run)
+    (define-key racket-mode-map (kbd "M-.") #'xref-find-definitions))
+
+  (add-hook 'racket-mode-hook #'racket-xp-mode)
+
+  (add-hook 'racket-mode-hook #'paredit-mode)
+  (add-hook 'racket-repl-mode-hook #'paredit-mode)
+
+  (add-hook 'racket-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'racket-repl-mode-hook #'rainbow-delimiters-mode)
+
+  (add-hook 'racket-mode-hook #'racket-unicode-input-method-enable)
+  (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
+
+  (add-to-list 'auto-mode-alist '("\\.rkt?\\'" . racket-mode))
+  (add-to-list 'auto-mode-alist '("\\.rkt\\'" . racket-mode)))
+
 ;; Lua
 (use-package lua-mode
   :ensure t
