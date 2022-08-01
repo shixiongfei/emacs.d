f7b48ceda96a084c0604690f0f825efd1bb2fb7e
shixiongfei <xiongfei.shi@icloud.com>
2022-08-01 12:11:12 +0800

    ABCL as default Lisp implementation

--- init.el
+++ init.el
@@ -609,12 +609,14 @@
             (abcl ("abcl") :coding-system utf-8-unix)))
 
     ;; select the default value from slime-lisp-implementations
-    (if (and (eq system-type 'darwin)
-             (executable-find "ccl64"))
-        ;; default to Clozure CL on macOS
-        (setq slime-default-lisp 'ccl64)
-      ;; default to SBCL on Linux and Windows
-      (setq slime-default-lisp 'sbcl))
+    (if (executable-find "abcl")
+        (setq slime-default-lisp 'abcl)
+      (if (and (eq system-type 'darwin)
+               (executable-find "ccl64"))
+          ;; default to Clozure CL on macOS
+          (setq slime-default-lisp 'ccl64)
+        ;; default to SBCL on Linux and Windows
+        (setq slime-default-lisp 'sbcl)))
 
     ;; set slime contribs
     (setq slime-contribs '(slime-fancy
