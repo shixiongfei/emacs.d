132eafe909b67a5984a49d1a04ab0ea4a78db149
shixiongfei <jenson.shixf@gmail.com>
2025-06-05 11:31:36 +0800

    Update init.el

--- init.el
+++ init.el
@@ -681,9 +681,9 @@
   (web-mode-css-indent-offset 2)
   (web-mode-code-indent-offset 2)
   :config
+  (add-to-list 'auto-mode-alist '("\\.htm?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.xhtml?\\'" . web-mode)))
 
 ;; Lua
 (use-package lua-mode
